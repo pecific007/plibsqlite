c9898b12d57ce4617e3c2be4c244ab291730a09b
Prashant <invizableprash@gmail.com>
2026-05-16 10:28:05 +0530

    Separated Table from the Database

--- plibsqlite.py
+++ plibsqlite.py
@@ -9,23 +9,29 @@ THIS MODULE PROVIDES BASIC C.U.R.D. FUNCTIONALITIES
 """
 
 
-class Database:
-    def __init__(self, database_name: str, name: str, **kwargs):
-        """This will initialise the table name and the columns/fields of a table."""
+class Table:
+    def __init__(self, name: str, fields: dict) -> None:
         self.name = name
-        self.fields = kwargs
+        self.fields = fields
+
+
+class Database:
+    def __init__(self, database_name: str):
+        """This will initialise the table name and the columns/fields of a table."""
+        self.tables = {}
         self.con = sqlite3.connect(database_name)
 
     def __del__(self):
         """This is a destructor that will close the connection (for now)"""
         self.con.close()
 
-    def create_table(self):
+    def create_table(self, name: str, **kwargs):
         """This function is used to create table with the name"""
+        table = Table(name, kwargs)
         stmt = []
-        stmt.append(f"CREATE TABLE IF NOT EXISTS {self.name}(")
-        for f in self.fields:
-            stmt += f"{f} {self.fields[f]}"
+        stmt.append(f"CREATE TABLE IF NOT EXISTS {name}(")
+        for f in kwargs:
+            stmt += f"{f} {kwargs[f]}"
             stmt.append(",")
 
         # All the pops are mostly to remove the "," at the last element Otherwise it thorws an error
@@ -34,6 +40,7 @@ class Database:
         sql_query = "".join(stmt)
         print("Executing: ", sql_query)
         self.con.execute(sql_query)
+        self.tables[name] = table
 
     def prep_where_statement(self, **kwargs):
         stmt = []
@@ -50,10 +57,10 @@ class Database:
         where_stmt = "".join(stmt)
         return where_stmt, data
 
-    def insert_into_table(self, **kwargs):
+    def insert_into_table(self, table_name: str, **kwargs):
         stmt = []  # This is the sql query statement -- easy to prepare this way
         data = []  # This to store data
-        stmt.append(f"INSERT INTO {self.name}(")
+        stmt.append(f"INSERT INTO {table_name}(")
         for f in kwargs:  # This to get all the column names
             stmt.append(f"{f}")
             stmt.append(",")
@@ -68,11 +75,14 @@ class Database:
         stmt.pop()
         stmt.append(")")
         sql_query = "".join(stmt)
-        print("Executing: ", sql_query)
+        print("Executing: ", sql_query, data)
         self.con.execute(sql_query, data)
         self.con.commit()
 
-    def select_from_table(self, columns: list | str, where: str = "", data: list = []):
+    def select_from_table(
+        self, table_name: str, columns: list | str, where: str = "", data: list = []
+    ):
+        table = self.tables[table_name]
         stmt = []
         stmt.append("SELECT ")
         # This for selecting all values
@@ -81,20 +91,20 @@ class Database:
                 print(
                     "Getting all the rows: Providing empty value for columns will by default select *"
                 )
-                stmt.append(f" * FROM {self.name}")
+                stmt.append(f" * FROM {table_name}")
                 if len(where) != 0:
                     stmt.append(where)
                 sql_query = "".join(stmt)
-                print("Executing: ", sql_query)
+                print("Executing: ", sql_query, data)
                 return self.con.execute(sql_query, data)
             else:
-                assert columns in self.fields
+                assert columns in table.fields
                 stmt.append(columns)
-                stmt.append(f" FROM {self.name}")
+                stmt.append(f" FROM {table_name}")
                 if len(where) != 0:
                     stmt.append(where)
                 sql_query = "".join(stmt)
-                print("Executing: ", sql_query)
+                print("Executing: ", sql_query, data)
                 return self.con.execute(sql_query, data)
 
         # This for selecting values from columns
@@ -103,87 +113,90 @@ class Database:
             stmt.append(",")
 
         stmt.pop()
-        stmt.append(f" FROM {self.name}")
+        stmt.append(f" FROM {table_name}")
         if len(where) != 0:
             stmt.append(where)
         sql_query = "".join(stmt)
-        print("Executing: ", sql_query)
+        print("Executing: ", sql_query, data)
         return self.con.execute(sql_query, data)
 
-    def delete_from_table(self, where: str = "", data: list = []):
+    def delete_from_table(self, table_name: str, where: str = "", data: list = []):
         """This function will delete from table."""
         # Empty where statement (i.e. where = "") it will drop table
         if len(where) == 0:
-            stmt = f"DROP TABLE {self.name}"
+            stmt = f"DROP TABLE {table_name}"
             self.con.execute(stmt)
             self.con.commit()
             return
 
-        stmt = f"DELETE FROM {self.name} {where}"
-        print("Executing: ", stmt)
+        stmt = f"DELETE FROM {table_name} {where}"
+        print("Executing: ", stmt, data)
         self.con.execute(stmt, data)
         self.con.commit()
 
-    def update_column_table(self, where: str, data: list, **kwargs):
+    def update_column_table(self, table_name: str, where: str, data: list, **kwargs):
         """Updating a column, WHERE statement is necessary"""
         stmt = []
-        stmt.append(f"UPDATE {self.name} SET ")
+        cols = []
+        stmt.append(f"UPDATE {table_name} SET ")
         for k in kwargs:
             stmt.append(f"{k} = ?")
             stmt.append(",")
-            data.append(kwargs[k])
+            cols.append(kwargs[k])
+        i = 0
+        for c in cols:
+            data.insert(i, c)
         stmt.pop()
         stmt.append(where)
         sql_query = "".join(stmt)
-        print("Executing: ", sql_query)
+        print("Executing: ", sql_query, data)
         self.con.execute(sql_query, data)
         self.con.commit()
 
 
 def test():
     # Creating table
-    db = Database(
-        "database.db",
+    db = Database("database.db")
+    print("Create table")
+    db.create_table(
         "things",
         ID="INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT",
         THING_NAME="TEXT NOT NULL",
         THING_DESC="TEXT NOT NULL",
     )
-    print("Create table")
-    db.create_table()
 
     # Adding values to table
     print("Insert values")
-    db.insert_into_table(THING_NAME="Air cooler", THING_DESC="For summer")
-    db.insert_into_table(THING_NAME="Table", THING_DESC="Wooden and used")
+    db.insert_into_table("things", THING_NAME="Air cooler", THING_DESC="For summer")
+    db.insert_into_table("things", THING_NAME="Table", THING_DESC="Wooden and used")
 
     # Selecting from table
     print("Select from table")
-    table_data = db.select_from_table("THING_NAME")
+    table_data = db.select_from_table("things", "THING_NAME")
     print("Table data: ", table_data.fetchall())
 
     # Updating values from table
     print("Update table")
-    where_stmt, where_data = db.prep_where_statement(ID=1)
-    db.update_column_table(where_stmt, where_data, THING_NAME="Wood Table")
+    where_stmt, where_data = db.prep_where_statement(ID=2)
+    db.update_column_table("things", where_stmt, where_data, THING_NAME="Wood Table")
 
     # Selecting from table with WHERE condition
     print("Selecting with WHERE condition")
     where_stmt, where_data = db.prep_where_statement(THING_NAME="Wood Table")
     # print("exmple:", where_stmt, where_data)
-    table_data = db.select_from_table("*", where_stmt, where_data)
+    table_data = db.select_from_table("things", "*", where_stmt, where_data)
     print("Table data: ", table_data.fetchall())
 
     # Deleting from table
     print("Deleting from table")
     where_stmt, where_data = db.prep_where_statement(ID=2)
-    db.delete_from_table(where_stmt, where_data)
-    table_data = db.select_from_table("*")
+    db.delete_from_table("things", where_stmt, where_data)
+    table_data = db.select_from_table("things", "*")
     print("Table data: ", table_data.fetchall())
 
     # Dropping the table
     print("Dropping table")
-    db.delete_from_table()
+    db.delete_from_table("things")
     db.__del__()
 
 
