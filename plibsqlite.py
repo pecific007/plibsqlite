cba5d1dfebfab096c35db0238ffafef0640e62ed
Prashant <invizableprash@gmail.com>
2026-05-21 13:47:43 +0530

    Added replace_placeholder, and exec

--- plibsqlite.py
+++ plibsqlite.py
@@ -20,20 +20,28 @@ class Database:
         """This will initialise the table name and the columns/fields of a table."""
         self.tables = {}
         self.con = sqlite3.connect(database_name)
+        self.IN_KEYWORD = "FOUND 'IN' OPERATOR"
         self.sync_with_database()
 
     def __del__(self) -> None:
         """This is a destructor that will close the connection (for now)"""
         self.con.close()
 
+    def exec(self, stmt: str, params: list = []) -> sqlite3.Cursor:
+        print("Executing: ", stmt, params)
+        return self.con.execute(stmt, params)
+
     def sync_with_database(self) -> None:
-        tables_full = self.con.execute(
-            "SELECT name FROM sqlite_master WHERE type='table'"
+        where_stmt, where_data = self.prep_where_statement("=", type="table")
+        tables_full = self.select_from_table(
+            "sqlite_master", "name", where_stmt, where_data
         )
         tables = tables_full.fetchall()
         # print(tables)
         for t in tables:
             td = (self.con.execute(f"PRAGMA table_info('{t[0]}')")).fetchall()
+            # for d in td:
+            #     print(d)
             new_table_fields = {
                 d[1]: f"{d[2]} PRIMARY KEY"
                 if d[5] == 1
@@ -42,36 +50,52 @@ class Database:
                 else f"{d[2]} NOT NULL"
                 for d in td
             }
+            # print(new_table_fields)
             new_table = Table(t[0], new_table_fields)
             self.tables[t[0]] = new_table
 
-    def create_table(self, name: str, **kwargs) -> None:
+    def create_table(self, name: str, ret_stmt: bool = False, **kwargs) -> None | str:
         """This function is used to create table with the name"""
         table = Table(name, kwargs)
         stmt = []
-        stmt.append(f"CREATE TABLE IF NOT EXISTS {name}(")
+        stmt.append(f'CREATE TABLE IF NOT EXISTS "{name}"(')
         for f in kwargs:
-            stmt += f"{f} {kwargs[f]}"
+            stmt += f'"{f}" "{kwargs[f]}"'
             stmt.append(",")
 
         # All the pops are mostly to remove the "," at the last element Otherwise it thorws an error
         stmt.pop()
         stmt.append(")")
         sql_query = "".join(stmt)
+        if ret_stmt:
+            return sql_query
         print("Executing: ", sql_query)
         self.con.execute(sql_query)
         self.tables[name] = table
 
     def prep_where_statement(self, operator: str, **kwargs):
-        possible_operators = ["=", "!=", ">", ">=", "<", "<=", "LIKE", "like"]
+        possible_operators = [
+            "=",
+            "!=",
+            ">",
+            ">=",
+            "<",
+            "<=",
+            "LIKE",
+            "like",
+            "IN",
+            "in",
+        ]
         if operator not in possible_operators:
             print("Please provide a valid operator!")
             return "", []
         stmt = []
         data = []
+        if operator == "IN" or operator == "in":
+            data.append(self.IN_KEYWORD)
         stmt.append(" WHERE ")
         for k in kwargs:
-            stmt.append(f"{k} {operator} ? ")
+            stmt.append(f'"{k}" {operator} (?) ')
             data.append(kwargs[k])
             stmt.append(" AND ")
         if stmt[(len(stmt) - 1)] == " AND ":
@@ -79,24 +103,28 @@ class Database:
         where_stmt = "".join(stmt)
         return where_stmt, data
 
-    def insert_into_table(self, table_name: str, **kwargs):
+    def insert_into_table(
+        self, table_name: str, ret_stmt: bool = False, **kwargs
+    ) -> None | str:
         stmt = []  # This is the sql query statement -- easy to prepare this way
         data = []  # This to store data
-        stmt.append(f"INSERT INTO {table_name}(")
+        stmt.append(f'INSERT INTO "{table_name}"(')
         for f in kwargs:  # This to get all the column names
-            stmt.append(f"{f}")
+            stmt.append(f'"{f}"')
             stmt.append(",")
         stmt.pop()
         stmt.append(")")
 
         stmt.append(" VALUES(")
         for k in kwargs:  # This to get the values
-            stmt.append("?")
+            stmt.append("(?)")
             stmt.append(",")
             data.append(kwargs[k])
         stmt.pop()
         stmt.append(")")
         sql_query = "".join(stmt)
+        if ret_stmt:
+            return sql_query
         print("Executing: ", sql_query, data)
         self.con.execute(sql_query, data)
         self.con.commit()
@@ -109,8 +137,13 @@ class Database:
         data: list = [],
         limit: int = 0,
         order_by: dict = {},
+        ret_stmt: bool = False,
     ):
         stmt = []
+        if data:
+            if data[0] == self.IN_KEYWORD:
+                data = data[1:]
+                data = data[0]
         stmt.append("SELECT ")
         # This for selecting all values
         if isinstance(columns, str) or len(columns) == 0:
@@ -118,21 +151,26 @@ class Database:
                 print(
                     "Getting all the rows: Providing empty value for columns will by default select *"
                 )
-                stmt.append(f" * FROM {table_name}")
+                stmt.append(f' * FROM "{table_name}"')
                 if len(where) != 0:
                     stmt.append(where)
                 sql_query = "".join(stmt)
             else:
                 stmt.append(columns)
-                stmt.append(f" FROM {table_name}")
+                stmt.append(f' FROM "{table_name}"')
                 if len(where) != 0:
                     stmt.append(where)
             if len(order_by) > 0:
+                stmt.append(" ORDER BY ")
                 for k in order_by:
-                    stmt.append(f" ORDER BY {k} {order_by[k]}")
+                    stmt.append(f"{k} {order_by[k]}")
+                    stmt.append(",")
+                stmt.pop()
             if limit != 0:
                 stmt.append(f"LIMIT {limit}")
             sql_query = " ".join(stmt)
+            if ret_stmt:
+                return sql_query
             print("Executing: ", sql_query, data)
             return self.con.execute(sql_query, data)
 
@@ -146,25 +184,35 @@ class Database:
         if len(where) != 0:
             stmt.append(where)
         sql_query = "".join(stmt)
+        if ret_stmt:
+            return sql_query
         print("Executing: ", sql_query, data)
         return self.con.execute(sql_query, data)
 
-    def delete_from_table(self, table_name: str, where: str = "", data: list = []):
+    def delete_from_table(
+        self, table_name: str, where: str = "", data: list = [], ret_stmt: bool = False
+    ):
         """This function will delete from table."""
         # Empty where statement (i.e. where = "") it will drop table
         if len(where) == 0:
             stmt = f"DROP TABLE {table_name}"
+            if ret_stmt:
+                return stmt
             print("Executing: ", stmt)
             self.con.execute(stmt)
             self.con.commit()
             return
 
-        stmt = f"DELETE FROM {table_name} {where}"
+        stmt = f'DELETE FROM "{table_name}" {where}'
+        if ret_stmt:
+            return stmt
         print("Executing: ", stmt, data)
         self.con.execute(stmt, data)
         self.con.commit()
 
-    def update_column_table(self, table_name: str, where: str, data: list, **kwargs):
+    def update_column_table(
+        self, table_name: str, where: str, data: list, ret_stmt: bool = False, **kwargs
+    ):
         """Updating a column, WHERE statement is necessary"""
         stmt = []
         cols = []
@@ -179,6 +227,23 @@ class Database:
         stmt.pop()
         stmt.append(where)
         sql_query = "".join(stmt)
+        if ret_stmt:
+            return stmt
         print("Executing: ", sql_query, data)
         self.con.execute(sql_query, data)
         self.con.commit()
+
+    def replace_placeholder_with_data(self, stmt: str, *args):
+        data = ""
+        new_stmt = ""
+        if self.IN_KEYWORD == args[0][0]:
+            data = args[0][1:]
+        else:
+            data = args[0]
+        i = 0
+        for s in stmt:
+            if s == "?":
+                s = data[i]
+                i += 1
+            new_stmt += f"{s}"
+        return new_stmt
